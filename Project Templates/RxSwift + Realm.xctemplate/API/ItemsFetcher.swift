e7f0e44d758920c3b52e1ef5817e6cabf99464f2
Chung Tran <bigearsenal@gmail.com>
2019-04-07 01:49:34 +0300

    Add items fetcher

---
+++ Project Templates/RxSwift + Realm.xctemplate/API/ItemsFetcher.swift
@@ -0,0 +1,63 @@
+//
+//  RepositoryFetcher.swift
+//  github
+//
+//  Created by Chung Tran on 26/03/2019.
+//  Copyright © 2019 Chung Tran. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import RealmSwift
+
+import Unbox
+
+class ItemsFetcher<T> where T: Unboxable {
+    enum FetcherError: Error {
+        case createRequestFailed, requestFailed, canceled, requestDupplicated
+    }
+    
+    // MARK: - Init
+    private var router: String
+    init(router: String) {
+        self.router = router
+    }
+    
+    // MARK: - Subjects
+    let list = BehaviorRelay<[T]>(value: [])
+    let bag = DisposeBag()
+    
+    // MARK: - Methods
+    var isFetching = false
+    var reachedTheEnd = false
+    var maxId: String?
+    
+    func refresh() {
+        reachedTheEnd = false
+        isFetching = false
+        maxId = nil
+    }
+    
+    func requestNext(mock: Bool = false) -> Single<[T]> {
+        // check if request is dupplicated
+        if self.isFetching || self.reachedTheEnd {
+            return Single.error(FetcherError.requestDupplicated)
+        }
+        
+        // mark as fetching
+        self.isFetching = true
+        
+        // for testing, add mock objects
+        if mock == true {
+            #warning("Add mock objects")
+            self.isFetching = false
+            self.reachedTheEnd = true
+            return Single.just([T]())
+        }
+        
+        // implement logic for retrieve real data (from network)
+        #warning("implement logic for retrieve real data (from network)")
+        return Single.just([T]())
+    }
+}
